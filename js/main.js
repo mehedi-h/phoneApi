1d7e1b6f58edf8ae9d3cda7909eb9e7d0d5a6be4
mehedi-h <mehedihs2015@gmail.com>
2023-08-28 20:09:07 +0600

    add modal

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const loadPhones = async (searchText, isShowAll) => {
     
     displayPhone(phones, isShowAll)
 }
-
+//--------------- Display Phones Function ------------------------>
 const displayPhone = (phones,isShowAll)  => {
     const phoneContainer = document.getElementById('phone-container')
     phoneContainer.textContent = '';
@@ -29,22 +29,31 @@ const displayPhone = (phones,isShowAll)  => {
 
         phoneCard.innerHTML = `
             <figure class="px-10 pt-10">
-                <img src="${phone.image}" alt="Phone" class="rounded-xl" />
+                <img src="${phone.image}" alt="${phone.phone_name}" class="rounded-xl" />
             </figure>
-            <div class="card-body items-center text-center">
+            <div class="card-body items-center justify-center">
                     <h2 class="card-title">${phone.phone_name}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                 <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button onClick='showDetails("${phone.slug}")' class="btn btn-primary">Show Details</button>
                 </div>
             </div>
         `
+        //--------------- Append Child ------------------------>
         phoneContainer.appendChild(phoneCard)
     });
-
+//--------------- Loading hide ------------------------>
     toggleSpinner(false)
 }
+//--------------- Show Details Function ------------------------>
+const showDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    const phone = data.data
+    console.log(phone)
+}
 
+//--------------- Search Button Function ------------------------>
 const searchBtn = (isShowAll) => {
     const searchField = document.getElementById('search-field')
     const searchInput = searchField.value;
@@ -52,12 +61,11 @@ const searchBtn = (isShowAll) => {
     toggleSpinner(true);
     loadPhones(searchInput, isShowAll);
 }
-
+//--------------- Show All Button Function ------------------------>
 const showAllBtn = () => {
-    
     searchBtn(true)
 }
-
+//--------------- Loading Spinner Function ------------------------>
 const toggleSpinner = (isLoading) => {
     const loadingBar = document.getElementById('loading-bar');
     if (isLoading) {
